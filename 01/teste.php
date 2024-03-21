0a27a500796cc7509b6cefc554b0dd267e966a28
luis@virtualBox <luisgrbenvindo@gmail.com>
2024-03-20 22:13:09 -0300

    Entendendendo a Sintaxe basica de PHP

--- 01/teste.php
+++ 01/teste.php
@@ -1,5 +1,37 @@
 <?php
 
-    echo"Olá Mundo";
+    echo"Olá Mundo! <br>";
+    //isso é um comentario
+    #isso é um comentario
 
+    /*
+    isso é um comentario de multiplas linhas 
+    EU disse,multiplas linhas
+    */
+
+    //Declarando variáveis
+
+    $nome = "João";
+    $sobrenome= "Silva";
+    $nome_completo = $nome." ".$sobrenome;
+
+    //echo $nome;
+    //echo $sobrenome;
+
+    echo $nome." ".$sobrenome."<br>";
+    echo $nome_completo."<br>";
+
+    //VAriaveis Numericas
+    $idade = 25;
+    $ano = 2024;
+
+    echo $idade . "<br>";
+    echo $ano - $idade."<br>";  // operações matemáticas
+
+    echo 'TEXTO COM ASPAS SIMPLES <br>';
+    echo "TExto com aspas duplas <br>";
+    //Trabalhando com aspas simples e duplas
+    echo 'Dom Pedro disse:"Independencia ou Morte!"<br>';
+
+    echo "$nome tem $idade anos de idade";
 ?>
