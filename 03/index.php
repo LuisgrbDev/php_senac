88463ee6d7030c16316b52f2095ee6dc207ee027
luisSenac <luisgrbenvindo@gmail.com>
2024-03-22 21:39:21 -0300

    AULA 03 - OPERADORES

--- 03/index.php
+++ 03/index.php
@@ -51,5 +51,54 @@ $num2 = 5;
 
 //IGUALDADE
 echo "Igual a : ";
-echo $num1 == $num2 ? "verdadeiro": "falso";
+echo $num1 == $num2 ? "verdadeiro <br>": "falso <br>";
+
+//DIFERENTE
+echo "Diferente de: ";
+echo $num1 != $num2 ? "Verdadeiro <br>": "Falso <br>";
+
+//MAIOR QUE 
+echo "Mairo que: ";
+echo $num1 > $num2 ? "Verdadeiro <br>": "Falso <br>";
+
+//MENOR QUE 
+echo "Menor que: ";
+echo  $num1 < $num2 ? "Verdadeiro <br>": "Falso <br>";
+
+echo "<hr>";
+//OPERADORES LÓGICOS
+
+//E LÓGICO (AND)
+
+echo "Verdadeiro e Verdadeiro: ";
+echo true && true ? "Verdadeiro <br>" : "Falso <br>";
+
+
+echo "Verdadeiro e Falso: ";
+echo true && false ? "verdadeiro <br>" : "falso <br>";
+
+
+echo "Falso e Verdadeiro: ";
+echo false && true ? "verdadeiro <br>" : "falso <br>";
+
+echo "Falso e Falso: ";
+echo false && false ? "verdadeiro <br>" : "falso <br>";
+
+// OU lógico (or)
+echo "Verdadeiro e Verdadeiro: ";
+echo true || true ? "verdadeiro <br>" : "falso <br>";
+
+echo "Verdadeiro e Falso: ";
+echo true || false? "verdadeiro <br>" : "falso <br>";
+
+echo "Falso e Verdadeiro: ";
+echo false || true ? "verdadeiro <br>" : "falso <br>";
+
+echo "Falso e Falso: ";
+echo false || false ? "verdadeiro <br>" : "falso <br>";
+
+//Não lógico (NOT)
+echo "Negando algo que é verdadeiro: ";
+echo !true ? "Verdadeiro <br>": "False <br>";
+
 ?>
