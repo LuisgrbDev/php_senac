92e1e19645ba457ed1d8608b2ca0367107bca5f2
luis@virtualBox <luisgrbenvindo@gmail.com>
2024-03-26 20:32:26 -0300

    AULA 05 ARRAYS

---
+++ 05/index.php
@@ -0,0 +1,54 @@
+<?php
+
+//INTRODUÇÃO A ARRAYS EM PHP
+
+// Definição de Arrays
+
+$frutas = array("Maça","Banana","Laranja","Morango");
+
+$numeros = [1,2,3,4,5];
+$alunos = array("João" => 18, "Maria" =>20,"Pedro" => 19);
+
+// Acessando elementos do Arrays
+
+
+echo "A segunda fruta é: ".$frutas[1]."<br>";
+echo "O terceiro numero é: ". $numeros[2]."<br>";
+
+echo "A idade de João é:  ".$alunos["João"]." Anos <br>";
+
+
+// Alterar elementos do Array
+
+$frutas[0] = "Pera";
+$numeros[4] = 10;
+$alunos["Maria"] = 21;
+
+//Adcionar elementos ao Array
+
+$frutas[] = "Abacaxi";
+$numeros[] = 6;
+$alunos["Ana"] = 22;
+
+// percorrendo um array com o foreach
+
+echo "Lista de Frutas: ";
+
+foreach($frutas as $frutas){
+  echo $frutas." ";
+}
+echo "<br>";
+
+echo "Lista de números: ";
+
+foreach($numeros as $numeros){
+    echo $numeros . " ";
+}
+
+echo "<br>";
+
+echo "Lista de Alunos:<br> ";
+foreach($alunos as $nome => $idade){
+  echo $nome ." tem ". $idade. " Anos.  <br>";
+}
+?>
