69a52b3022b2e8407f779ff63d9f088eb60722be
luisgrbenvindo@gmail.com <luisgrbenvindo@gmail.com>
2024-04-04 20:00:08 -0300

    Aula 12 - final

--- 12/index.php
+++ 12/index.php
@@ -43,15 +43,32 @@
     }
 
     class Guerreiro extends Personagem{
+      private $poderEscudo;
+      private $ataqueEspada;
+
+      public function __construct($nome, $pontosVida,$poderAtaque,$poderEscudo){
+        parent::__construct($nome, $pontosVida,$poderAtaque);
+        $this->poderEscudo = $poderEscudo;
+        $this->ataqueEspada = 5;
+      }
         public function atacar($inimigo){
-
+                $dano = $this->poderAtaque + $this->ataqueEspada;
+                $inimigo->sofrerDano($dano);
         }
-
     }
 
     class Mago extends Personagem{
+        private $pontosMagia;
+
+        public function __construct($nome,$pontosVida,$poderAtaque,$pontosMagia)
+        {
+            parent::__construct($nome,$pontosVida,$poderAtaque);
+            $this->pontosMagia = $pontosMagia;
+        }
+
         public function atacar($inimigo){
-            
+          $inimigo->sofrerDano($this->poderAtaque);
+          $this->poderAtaque -= $this->pontosMagia;
         }
 
     }
@@ -59,8 +76,8 @@
     //Não é possivel instanciar um objeto a partir de uma classe abstrata
     //$personagem1 = new Personagem();
 
-    $guerreiro = new Guerreiro("Aragorn",100,20);
-    $mago = new Mago("Gandalf",80,30);
+    $guerreiro = new Guerreiro("Aragorn",100,20,10);
+    $mago = new Mago("Gandalf",80,30,100);
   
     echo "Dados do Guerreiro: <br>";
     echo $guerreiro->exibirDados();
@@ -68,7 +85,15 @@
     echo $mago->exibirDados();
 
 
-    $guerreiro->sofrerDano(120);
-    echo "Sofreu dano de 120";
+    // $guerreiro->sofrerDano(120);
+    // echo "Sofreu dano de 120";
+    // $guerreiro->exibirDados();
+
+    echo "Guerreiro atacou o mago <br>";
+    $guerreiro->atacar($mago);
+    $mago->exibirDados();
+
+    echo "Mago atacou guerreiro<br>";
+    $mago->atacar($guerreiro);
     $guerreiro->exibirDados();
 ?>
