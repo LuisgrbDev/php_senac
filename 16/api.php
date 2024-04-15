6b084fe6443f6479e514b39d7d3df77c35709f9d
luis@virtualBox <luisgrbenvindo@gmail.com>
2024-04-15 20:56:53 -0300

    AULA 16

---
+++ 16/api.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'DatabaseRepository.php';
+
+//print_r(DatabaseRepository::getContactById(1));
+
+//echo DatabaseRepository::updateContact(1, "Bruno Chaveiro", "1194585235","bruninhodachave@gmail.com");
+ echo DatabaseRepository::deleteContact(5);
+?>
