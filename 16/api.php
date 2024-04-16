8b5a5c9820132377fcfa0001cc8571a07396b38f
luis@virtualBox <luisgrbenvindo@gmail.com>
2024-04-15 22:16:01 -0300

    AULA 16: API

--- 16/api.php
+++ 16/api.php
@@ -1,8 +1,33 @@
 <?php
 require_once 'DatabaseRepository.php';
 
-//print_r(DatabaseRepository::getContactById(1));
+$action  =$_GET['action'];
 
-//echo DatabaseRepository::updateContact(1, "Bruno Chaveiro", "1194585235","bruninhodachave@gmail.com");
- echo DatabaseRepository::deleteContact(5);
+
+switch($action){
+    case 'list':
+        echo json_encode(DatabaseRepository::getAllContacts());
+        break;
+    case 'add':
+        $data = json_decode(file_get_contents('php://input',true));
+        $sucess = DatabaseRepository::InsertContact($data->nome,$data->telefone,$data->email);
+        echo json_encode(['Success' => $sucess]);
+        break;
+
+    case 'update': 
+        $data = json_decode(file_get_contents('php://input',true));
+        $success =  DatabaseRepository::updateContact($data->id,$data->nome,$data->telefone,$data->email);
+        echo json_encode(['Success' => $success]);
+        break;
+
+    case 'delete':
+        $id =  $_GET['id'];
+        $sucess = DatabaseRepository::deleteContact($id);
+        echo json_encode(['success' => $sucess]);
+        break;
+
+    default: 
+    echo json_encode(['ERROR'=>'Ação inválida']);
+
+}
 ?>
