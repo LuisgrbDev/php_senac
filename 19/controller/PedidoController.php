c48f1be68037d31dc6b7ad2eea2756fbda3b07e3
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-04-29 22:08:21 -0300

    AULA 19 : exercicios finalizados

--- 19/controller/PedidoController.php
+++ 19/controller/PedidoController.php
@@ -2,23 +2,99 @@
 
 require_once 'database/PedidoRepository.php';
 
-class PedidoController{
-   
-    public static function handleRequest($action){
-        switch($action){
+class PedidoController
+{
+
+    public static function handleRequest($action)
+    {
+        switch ($action) {
             case 'listar':
-               self::listarPedidos();
+                self::listarPedidos();
                 break;
-                default:
+            case 'buscar':
+                self::buscarPorId();
+                break;
+            case 'buscar_pedido':
+                self::listarPedidosProduto();
+                break;
+            case 'cadastrar':
+                self::CadastrarPedido();
+                break;
+            case 'atualizar':
+                self::atualizarPedido();
+                break;
+            case 'excluir':
+                self::excluirPedido();
+                break;
+
+
+            default:
                 http_response_code(400); // Requisição inválida
-                echo json_encode(['error'=> 'Ação invalida']);
+                echo json_encode(['error' => 'Ação invalida']);
                 break;
         }
     }
 
-    public static function listarPedidos(){
+    public static function listarPedidos()
+    {
         $pedidos =  PedidoRepository::getAllPedidos();
         echo json_encode($pedidos);
     }
+
+    public static function buscarPorId()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $id = $_GET['id'];
+            $pedidod = PedidoRepository::getBuscarPorId($id);
+            if ($pedidod) {
+                echo json_encode($pedidod);
+            } else {
+                http_response_code(404);
+                echo json_encode(['error' => 'Entidade Invalida']);
+            }
+        } else {
+            http_response_code(405);
+        }
+    }
+
+    public  static function CadastrarPedido()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data = json_decode(file_get_contents("php://input"));
+            $pedido = new Pedido(null, $data->data_pedido, $data->status);
+            $success = PedidoRepository::inserirPedidos($pedido);
+            echo json_encode(['Succes' => $success]);
+        } else {
+            http_response_code(405);
+        }
+    }
+
+    public static function atualizarPedido()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data =  json_decode(file_get_contents("php://input"));
+            $pedido = new Pedido($data->id, $data->data_pedido, $data->status);
+            $success = PedidoRepository::atualizarPedido($pedido);
+            echo json_encode(['succes' => $success]);
+        } else {
+            http_response_code(405);
+        }
+    }
+
+    public static function excluirPedido()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $data =  json_decode(file_get_contents("php://input"));
+            $id = $data->id;
+            $success = PedidoRepository::deletePedido($id);
+            echo json_encode(['success' => $success]);
+        } else {
+            http_response_code(405);
+        }
+    }
+    public static function listarPedidosProduto()
+    {
+        $pedidos_produto =  PedidoRepository::buscarPedidoProduto();
+        echo json_encode($pedidos_produto);
+    }
 }
-?>
--- 19/database/pedidorepository.php
+++ 19/database/pedidorepository.php
@@ -26,7 +26,7 @@ class PedidoRepository{
         $pedido = null;
         if($result->num_rows > 0){
             $row = $result->fetch_assoc();
-            $pedido=new Pedido($row['id'], $row['nome'], $row['data_pedido'], $row['status']);
+            $pedido=new Pedido($row['id'], $row['data_pedido'], $row['status']);
         }
         $connection->close();
         return $pedido; 
@@ -37,16 +37,50 @@ class PedidoRepository{
         $id = $pedido->getId();
         $data = $pedido->getData();
         $status = $pedido->getStatus();
-        $sql = "INSERT INTO pedido VALUES ($id,$data,$status)";
+        $sql = "INSERT INTO pedido (data_pedido,status) VALUES ('$data','$status')";
         $success = $connection->query($sql);
         $connection->close();
         return $success;
     }
     
-    public static function atualizarPedido(){
+    public static function atualizarPedido(Pedido $pedido){
         $connection = DatabaseRepository::connect();
+        $id = $pedido->getId();
+        $data = $pedido->getData();
+    
+        $status = $pedido->getStatus();
+        echo "id: $id, data: $data status: $status";
+        $sql = "UPDATE pedido SET data_pedido = '$data', status = '$status' WHERE id= $id";
+        $success = $connection->query($sql);
+        
+        $connection->close();
+        return $success;
     }
+    
+    public static function deletePedido($id){
+        $connection = DatabaseRepository::connect();
+        $success = $connection->query("DELETE pedido WHERE id = $id");
+        $connection->close();
+        return $success;
+    }
+    
+    public static function buscarPedidoProduto(){
+        $connection = DatabaseRepository::connect();
+        $sql = "SELECT * FROM produto_pedido 
+        INNER JOIN produto ON produto_pedido.produto_id = produto.id 
+        INNER JOIN pedido ON produto_pedido.pedido_id = pedido.id";
+         $success = $connection->query($sql);
+         $produtoPedido = [];
+        if($success->num_rows > 0 ){
+            while($row = $success->fetch_assoc()){
+                $produtoPedido[] = $row;
+            }
+        }
+       
+        $connection->close();
+        return $produtoPedido;
 
+    }
 }
 
 
